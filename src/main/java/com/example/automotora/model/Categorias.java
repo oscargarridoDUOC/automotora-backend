79123e5d2df0222522b44140ed18bb82bf34b559
Oscar Garrido <osca.garrido@duocuc.cl>
2025-11-12 17:52:57 -0300

    Cración Categorias + Avance MarcaService

--- src/main/java/com/example/automotora/model/Categorias.java
+++ src/main/java/com/example/automotora/model/Categorias.java
@@ -1,6 +1,5 @@
 package com.example.automotora.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
--- src/main/java/com/example/automotora/service/MarcaService.java
+++ src/main/java/com/example/automotora/service/MarcaService.java
@@ -1,5 +1,47 @@
 package com.example.automotora.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.automotora.model.Marca;
+import com.example.automotora.repository.MarcaRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
 public class MarcaService {
+
+    @Autowired
+    private MarcaRepository marcaRepository;
+
+    public List<Marca> getAllMarcas() {
+        return marcaRepository.findAll();
+    }
+
+    public Marca getMarcaById(Integer id) {
+        return marcaRepository.findById(id).orElse(null);
+    }
+
+    public Optional<Marca> getMarcasByNombre(String nombre) {
+        return marcaRepository.findByNombre(nombre);
+    }
+
+    public Marca saveMarca(Marca marca) {
+        return marcaRepository.save(marca);
+    } 
+
+    public void deleteMarca(Integer id) {
+        marcaRepository.deleteById(id);
+
+        //TODO: VERIFICAR CASCADE
+    }
+
+    //TODO: CREAR UPDATE MARCA
+
+    //TODO: CREAR PATCH MARCA
 
 }
