42851507030ebab7adbc7cd670fd969f5558a424
Oscar Garrido <osca.garrido@duocuc.cl>
2025-11-12 22:06:43 -0300

    Implementacion Service

--- src/main/java/com/example/automotora/service/MarcaService.java
+++ src/main/java/com/example/automotora/service/MarcaService.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.automotora.model.Marca;
+import com.example.automotora.model.Vehiculo;
 import com.example.automotora.repository.MarcaRepository;
+import com.example.automotora.repository.VehiculoRepository;
 
 import jakarta.transaction.Transactional;
 
@@ -17,6 +19,9 @@ public class MarcaService {
 
     @Autowired
     private MarcaRepository marcaRepository;
+
+    @Autowired
+    private VehiculoRepository vehiculoRepository;
 
     public List<Marca> getAllMarcas() {
         return marcaRepository.findAll();
@@ -32,16 +37,31 @@ public class MarcaService {
 
     public Marca saveMarca(Marca marca) {
         return marcaRepository.save(marca);
-    } 
-
-    public void deleteMarca(Integer id) {
-        marcaRepository.deleteById(id);
-
-        //TODO: VERIFICAR CASCADE
     }
 
-    //TODO: CREAR UPDATE MARCA
+    public Marca updateMarca(Integer id, Marca marcaActualizada) {
+        Marca marca = marcaRepository.findById(id).orElse(null);
+        if (marca != null) {
+            marca.setNombre(marcaActualizada.getNombre());
+            return marcaRepository.save(marca);
+        }
+        return null;
+    }
 
-    //TODO: CREAR PATCH MARCA
+    public Marca patchMarca(Integer id, Marca marcaParcial) {
+        Marca marca = marcaRepository.findById(id).orElse(null);
+        if (marca != null) {
+            if (marcaParcial.getNombre() != null) {
+                marca.setNombre(marcaParcial.getNombre());
+            }
+            return marcaRepository.save(marca);
+        }
+        return null;
+    }
 
+    public void deleteMarca(Integer id) {
+        List<Vehiculo> vehiculos = vehiculoRepository.findByMarcaId(id);
+        vehiculoRepository.deleteAll(vehiculos);
+        marcaRepository.deleteById(id);
+    }
 }
